bc1ef4a2a241b4b137ed31c35dd899f4766a2885
Mateo Ferradans <mateo.ferradans@mercadolibre.com>
2025-05-29 21:26:08 -0300

    init: proyecto backend base

---
+++ MypresentpastBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mypresentpastbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypresentpastBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypresentpastBackendApplication.class, args);
+    }
+
+}
