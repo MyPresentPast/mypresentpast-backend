3c051ccb67b44290e5545a679608cac4899b8270
Nicolas Mamani <nicolasmamaniportal@outlook.com.ar>
2025-10-13 16:28:22 -0300

    fix acentos base de datos

--- setup-postgresql.sql
+++ setup-postgresql.sql
@@ -3,7 +3,12 @@
 -- ===================================
 
 -- Crear base de datos
-CREATE DATABASE mypresentpast_db;
+DROP DATABASE IF EXISTS mypresentpast_db;
+CREATE DATABASE mypresentpast_db
+  WITH ENCODING 'UTF8'
+  LC_COLLATE='es_AR.UTF-8'
+  LC_CTYPE='es_AR.UTF-8'
+  TEMPLATE=template0;
 
 -- Crear usuario (opcional)
 -- CREATE USER mypresentpast_user WITH PASSWORD 'mypresentpast_password';
